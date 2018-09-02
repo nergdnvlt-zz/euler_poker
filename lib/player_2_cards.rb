9f9f554966efc4edea882fd260cc6fef3d8907a8
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-02 15:57:33 -0600

    Still need to refactor tie breaker methods. Also thinking about player_1 and player_2 cards and if I want more generalized methods where the player is passed as an arguement, I think I prefer having the name of the method explicitly spell out who the eval is for, but I also like the idea of streamlining all those methods into one module, and think that passing the player might be explicit enough. I'll think about it tonight.

--- lib/player_2_cards.rb
+++ lib/player_2_cards.rb
@@ -1,2 +1,57 @@
 module Player2Cards
+  def player_2_4_kind_height
+    player_2.ranks.index(player_2.group_values[4][0])
+  end
+
+  def player_2_3_kind_height
+    player_2.ranks.index(player_2.group_values[3][0])
+  end
+
+  def player_2_two_pair_highest
+    player_2.ranks.index(player_2.group_values[2][-1])
+  end
+
+  def player_2_two_pair_next_highest
+    player_2.ranks.index(player_2.group_values[2][0])
+  end
+
+  def player_2_two_pair_other_card
+    player_2.ranks.index(player_2.group_values[1][0])
+  end
+
+  def player_2_single_pair_height
+    player_2.ranks.index(player_2.group_values[2][0])
+  end
+
+  def player_2_highest_single_card
+    player_2.ranks.index(player_2.group_values[1][-1])
+  end
+
+  def player_2_next_highest_single_card
+    player_2.ranks.index(player_2.group_values[1][-2])
+  end
+
+  def player_2_lowest_single_card
+    player_2.ranks.index(player_2.group_values[1][0])
+  end
+
+  def player_2_high_card
+    player_2.ranks.index(player_2.cards[-1].value)
+  end
+
+  def player_2_second_high_card
+    player_2.ranks.index(player_2.cards[-2].value)
+  end
+
+  def player_2_third_high_card
+    player_2.ranks.index(player_2.cards[-3].value)
+  end
+
+  def player_2_fourth_high_card
+    player_2.ranks.index(player_2.cards[-4].value)
+  end
+
+  def player_2_lowest_card
+    player_2.ranks.index(player_2.cards[0].value)
+  end
 end
