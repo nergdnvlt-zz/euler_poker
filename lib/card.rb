ae1ad0548dd847de9603b0e34fa8f5704bbaf8f6
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-02 15:59:15 -0600

    Revising git message.

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,8 @@
 class Card
-  attr_reader :value, :suit, :card
-  
+  attr_reader :value,
+              :suit,
+              :card
+
   def initialize(card_value)
     @value = card_value.split('')[0]
     @suit = card_value.split('')[1]
